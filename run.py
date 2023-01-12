5c8a0a22afe2c69f32b2ad22941bbcff27f7f333
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-01-12 11:58:17 +0000

    Add initial text to owenLeft section

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #Controls 
 yes_no = ["yes","no"]
-directions = ["left","right","forward","backwards"]
+directions = ["left","right","forward"]
 answers = [1,2,3]
 
 #Gamestart & Intro 
@@ -27,39 +27,53 @@ def introGame():
     while response not in yes_no: 
         response = input("Jon: Have you visted here before?\n")
         if response == "yes":
-            print("Jon: Ah excellent! I knew I recognized your ugly mug.\n No need for instructions then! Off you pop now.")
+            print("Jon: Ah excellent! I knew I recognized your ugly mug.")
+            print("Jon: No need for instructions then! Off you pop now.")
         elif response == "no":
             print("Jon: Ah a newbie! Well, let me impart some wisdom")
             print("Jon: You are here to collect the 3 HLL keys.")
-            print("Jon: There is no other way out of the Tomb.\n Except that fire exit door over there.")
-            print("Jon: You will meet 3 strangers.\n Each will pose you a riddle.")
-            print("Jon: Answering a riddle correctly will earn you a key and make you feel all smug and fuzzy.")
-            print("Jon: Once you've collected all 3 keys, you will be able to venture to....")
-            print("Jon: THE TEMPLE OF THE PODS!!!")
-            print("Some smoke went off behind him and dramatic music played from an old cd player behind a tree.")
-            print("Jon: Ahem, anyway. On you pop now.")
+            print("Jon: There is no other way out of the Tomb.")
+            print("Jon: Except that fire exit door over there.")
+            print("Jon: You will meet 3 strangers.")
+            print("Jon: Each will pose you a riddle.")
+            print("Jon: Answering a riddle correctly will earn you a key.")
+            print("Jon:...and make you feel all smug and fuzzy inside.")
+            print("Jon: Once you've collected all 3 keys")
+            print("Jon: you will be able to venture to....")
+            print("Jon: THE TEMPLE OF THE PODS!!!\n")
+            print("Some smoke went off behind him & dramatic music played.\n")
+            print("Jon: Ahem, anyway. Off you pop now.\n")
         else:
             print("Jon: Sorry, I didn't understand that.")
 
     print("The shadowy (and very handsome) stranger disappears.")
     print("There is a crossroads at the inn.")
-    print("Each road leading to a different tomb.")
+    print("Each road leading to a different tomb.\n")
+    print("Which way will you go?")
 
     while response not in directions: 
-        response = input("Which way will you go?\n left, right, forward, backwards")
-
-
-
-
-
-
+        response = input("left, right, forward \n")
+        if response == "left":
+            owenLeft()
+        elif response == "right":
+            liamRight()
+        elif response == "forward":
+            kevinForward()
+        else:
+            print("I'm sorry that is not a valid path.")
 
 
 def owenLeft():
     """
     The room to the left with the stranger named Owen
     """
-
+    directions = ["left"]
+    print("You decide to take the path to the left.")
+    print("It is a long dirt path with bare trees on either side.")
+    print("Up ahead, you see a small house made entirely of maple syrup")
+    print("It appears to be melting and reforming all at once")
+    print("A figure suddenly appears from the door, it is Owen.")
+    print("Owen: Well hello there eh. You lost eh?")
 
 def liamRight():
     """
@@ -72,7 +86,12 @@ def kevinForward():
     The room straight ahead with the stranger named Kevin
     """
 
-
+def jonInn():
+    """
+    Going backwards brings you back to the inn"
+    """
+    print("Jon: What are you doing back here? Onwards traveller!")
+    
 
 
 def main(): 
