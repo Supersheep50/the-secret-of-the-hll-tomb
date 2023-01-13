182a639a677f58b79f7588afdf35ccb54f3596f4
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-01-13 10:42:54 +0000

    Add slow print function to code and emojis

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
+"""
+Credit to replit
+"""
+import sys,time
+def delprint(text="Type a string in",delay_time=.05): 
+  for character in text:      
+    sys.stdout.write(character) 
+    sys.stdout.flush()
+    time.sleep(delay_time)
+
 import emoji
 
 #Controls 
@@ -11,47 +21,47 @@ def introGame():
     """
     Starts game and intoduces user to the game
     """
-    print("You stand in front of an old stone passageway.")
-    print("You push on the wall and it slowly slides open. You step inside.")
-    print("It is a huge cave.") 
-    print("However, open fields & trees stretch far into the distance")
-    print("To your right is a small building made of timber and straw.") 
-    print("Smoke is billowing from the chimney & the lights are on")
-    print("Suddenly a strange but very handsome man in dark robes appears.\n")
-    print("Jon: Welcome traveller!")
-    print("Jon: You look weary? Long Uber?")
-    print("Jon: My name is Jon and I will be your guide through this Tomb!")
-    print("Jon: Now, what is your name?\n")
+    delprint("You stand in front of an old stone passageway.\n")
+    delprint("You push on the wall and it slowly slides open. You step inside.\n")
+    delprint("It is a huge cave.\n") 
+    delprint("However, open fields & trees stretch far into the distance.\n")
+    delprint("To your right is a small building made of timber and straw.\n") 
+    delprint("Smoke is billowing from the chimney & the lights are on.\n")
+    delprint("Suddenly a strange but very handsome man in dark robes appears.\n")
+    delprint("Jon: Welcome traveller!\n")
+    delprint("Jon: You look weary? Long Uber?\n")
+    delprint("Jon: My name is Jon and I will be your guide through this Tomb!\n")
+    delprint("Jon: Now, what is your name?\n")
     name = input()
-    print("Jon: Nice to meet you "+name+".")
+    delprint("Jon: Nice to meet you "+name+".\n")
 
     response = ""
     while response not in yes_no: 
         response = input("Jon: Have you visted here before?\n")
         if response == "yes":
-            print("Jon: Ah excellent! I knew I recognized your ugly mug.")
-            print("Jon: No need for instructions then! Off you pop now.")
+            delprint("Jon: Ah excellent! I knew I recognized your ugly mug.\n")
+            delprint("Jon: No need for instructions then! Off you pop now.\n")
         elif response == "no":
-            print("Jon: Ah a newbie! Well, let me impart some wisdom")
-            print("Jon: You are here to collect the 3 HLL keys.")
-            print("Jon: There is no other way out of the Tomb.")
-            print("Jon: Except that fire exit door over there.")
-            print("Jon: You will meet 3 strangers.")
-            print("Jon: Each will pose you a riddle.")
-            print("Jon: Answering a riddle correctly will earn you a key.")
-            print("Jon:...and make you feel all smug and fuzzy inside.")
-            print("Jon: Once you've collected all 3 keys")
-            print("Jon: you will be able to venture to....")
-            print("Jon: THE TEMPLE OF THE PODS!!!\n")
-            print("Some smoke went off behind him & dramatic music played \U0001F3B6 \n")
-            print("Jon: Ahem, anyway. Off you pop now.\n")
+            delprint("Jon: Ah a newbie! Well, let me impart some wisdom.\n")
+            delprint("Jon: You are here to collect the 3 HLL keys.\n")
+            delprint("Jon: There is no other way out of the Tomb.\n")
+            delprint("Jon: Except that fire exit door over there.\n")
+            delprint("Jon: You will meet 3 strangers.\n")
+            delprint("Jon: Each will pose you a riddle.\n")
+            delprint("Jon: Answering a riddle correctly will earn you a key.\n")
+            delprint("Jon:...and make you feel all smug and fuzzy inside.\n")
+            delprint("Jon: Once you've collected all 3 keys.\n")
+            delprint("Jon: you will be able to venture to....\n")
+            delprint("Jon: THE TEMPLE OF THE PODS!!!\n")
+            delprint("Some smoke went off behind him & dramatic music played \U0001F3B6 \n")
+            delprint("Jon: Ahem, anyway. Off you pop now.\n")
         else:
-            print("Jon: Sorry, I didn't understand that.")
+            delprint("Jon: Sorry, I didn't understand that.\n")
 
-    print("The shadowy (and very handsome) stranger disappears.")
-    print("There is a crossroads at the inn.")
-    print("Each road leading to a different tomb.\n")
-    print("Which way will you go?")
+    delprint("The shadowy (and very handsome) stranger disappears.\n")
+    delprint("There is a crossroads at the inn.\n")
+    delprint("Each road leading to a different tomb.\n")
+    delprint("Which way will you go?\n")
 
     while response not in directions: 
         response = input("left, right, forward \n")
@@ -62,7 +72,7 @@ def introGame():
         elif response == "forward":
             kevinForward()
         else:
-            print("I'm sorry that is not a valid path.")
+            delprint("I'm sorry that is not a valid path.\n")
 
 
 def owenLeft():
@@ -70,12 +80,12 @@ def owenLeft():
     The room to the left with the stranger named Owen
     """
     directions = ["left"]
-    print("You decide to take the path to the left.")
-    print("It is a long dirt path with bare trees on either side.")
-    print("Up ahead, you see a small house made entirely of maple syrup")
-    print("It appears to be melting and reforming all at once")
-    print("A figure suddenly appears from the door, it is Owen.")
-    print("Owen: Well hello there eh. You lost eh?")
+    delprint("You decide to take the path to the left.\n")
+    delprint("It is a long dirt path with bare trees on either side.\n")
+    delprint("Up ahead, you see a small house made entirely of maple syrup.\n")
+    delprint("It appears to be melting and reforming all at once.\n")
+    delprint("A figure suddenly appears from the door, it is Owen.\n")
+    delprint("Owen: Well hello there eh. You lost eh?\n")
 
 def liamRight():
     """
@@ -92,7 +102,7 @@ def jonInn():
     """
     Going backwards brings you back to the inn"
     """
-    print("Jon: What are you doing back here? Onwards traveller!")
+    delprint("Jon: What are you doing back here? Onwards traveller!")
     
 
 
@@ -103,9 +113,10 @@ def main():
     introGame()
 
 
-print("Welcome to The Secret of the HLL Tomb!")
-print("The Secret of the HLL Tomb is a text adventure videogame.")
-print("Your goal is to collect 3 keys to open the door at the end of the Tomb.")
-print("You will meet 3 strangers along the way, each posing a riddle for you to answer")
+delprint("Welcome to The Secret of the HLL Tomb!\n")
+delprint("The Secret of the HLL Tomb is a text adventure videogame.\n")
+delprint("Your goal is to collect the 3 HLL keys \U0001F5DD.\n")	
+delprint("Together, they will open a door at the end of the tomb.\n")
+delprint("You will meet 3 strangers along the way, each posing a riddle.\n")
 print("Good luck!\n")
 main()
